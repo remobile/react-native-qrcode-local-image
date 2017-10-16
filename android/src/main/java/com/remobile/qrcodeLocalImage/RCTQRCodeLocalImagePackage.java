786b2ab619278795ae93b38ddafc89d76eadd0a1
fov42550564 <42550564@qq.com>
2017-10-16 11:41:02 +0800

    update to 1.0.1

--- android/src/main/java/com/remobile/qrcodeLocalImage/RCTQRCodeLocalImagePackage.java
+++ android/src/main/java/com/remobile/qrcodeLocalImage/RCTQRCodeLocalImagePackage.java
@@ -18,7 +18,6 @@ public class RCTQRCodeLocalImagePackage implements ReactPackage {
         );
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
